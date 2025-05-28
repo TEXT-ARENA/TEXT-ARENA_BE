bcee9d0935470057423a775fee36cfcc4fdc4433
m020202 <m020202@naver.com>
2025-05-28 15:07:26 +0900

    Initial Commit

---
+++ src/main/java/cc/team3/TextBattleApplication.java
@@ -0,0 +1,13 @@
+package cc.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/team3/TextBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
