d423af508154c85696099a71ea84d02296509f6e
m020202 <m020202@naver.com>
2025-06-04 10:11:04 +0900

    [CHORE] #11: exception 관련 처리 클래스 추가

---
+++ src/main/java/cc/team3/global/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package cc.team3.global.apiPayload.exception;
+
+import cc.team3.global.apiPayload.code.BaseErrorCode;
+import cc.team3.global.apiPayload.dto.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
