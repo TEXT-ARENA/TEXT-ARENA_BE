c6a2bbc364fd2072bdbefabaef0f1128795cc3df
m020202 <m020202@naver.com>
2025-06-05 13:10:55 +0900

    [FEAT] #20: 장비 생성 및 조회 로직 추가

---
+++ src/main/java/cc/team3/character/service/EquipmentService.java
@@ -0,0 +1,32 @@
+package cc.team3.character.service;
+
+import cc.team3.character.converter.EquipmentConverter;
+import cc.team3.character.domain.Equipment;
+import cc.team3.character.dto.CharacterResponse;
+import cc.team3.character.repository.EquipmentRepository;
+import cc.team3.global.apiPayload.exception.GeneralException;
+import cc.team3.global.apiPayload.status.ErrorStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EquipmentService {
+    private final EquipmentRepository equipmentRepository;
+
+    @Transactional
+    public String createEquipment(CharacterResponse.EquipmentDTO equipmentDTO) {
+        Equipment equipment = EquipmentConverter.toEquipment(equipmentDTO);
+        equipmentRepository.save(equipment);
+
+        return equipment.getId();
+    }
+
+    public List<Equipment> findEquipments (List<String> equipmentIds) {
+        return equipmentRepository.findAllById(equipmentIds);
+    }
+
+}
