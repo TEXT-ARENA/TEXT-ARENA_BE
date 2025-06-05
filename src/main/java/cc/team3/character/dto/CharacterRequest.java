cb2b4c360cb8f7b8e2793273d09ebc342d065f16
m020202 <m020202@naver.com>
2025-06-05 16:26:23 +0900

    [FEAT] #20: 무기 생성 요청 DTO 생성

--- src/main/java/cc/team3/character/dto/CharacterRequest.java
+++ src/main/java/cc/team3/character/dto/CharacterRequest.java
@@ -3,4 +3,7 @@ package cc.team3.character.dto;
 public class CharacterRequest {
     public record CharacterCreateRequestDTO(String characterName, String description) {
     }
+
+    public record CreateEquipmentRequestDTO(String equipmentType, String description) {
+    }
 }
