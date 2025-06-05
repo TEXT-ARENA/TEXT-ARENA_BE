91d802d9ced5dc865a77e1f5e62026471ed1458e
m020202 <m020202@naver.com>
2025-06-05 16:51:23 +0900

    [DOCS] #20: 장비 생성 API 관련 description 추가

--- src/main/java/cc/team3/character/controller/CharacterController.java
+++ src/main/java/cc/team3/character/controller/CharacterController.java
@@ -4,6 +4,7 @@ import cc.team3.character.dto.CharacterRequest;
 import cc.team3.character.dto.CharacterResponse;
 import cc.team3.character.service.CharacterService;
 import cc.team3.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,8 @@ public class CharacterController {
         return ApiResponse.onSuccess(characterService.createCharacter(userId, request));
     }
 
+    @Operation(summary = "장비 생성", description = "장비를 생성하고자 하는 캐릭터  ID를 넣어주세요 ! <br><br>" +
+        "equipmentType: 무기, 모자, 상의, 신발 중 하나 입력해주세요 ! <br> equipmentName: 장비 이름 <br> description: 장비 설명")
     @PostMapping("/{characterId}/equipments")
     public ApiResponse<CharacterResponse.CharacterDetailsResponseDTO> createWeapon(@RequestParam("characterId") Long characterId, @RequestBody CharacterRequest.CreateEquipmentRequestDTO request) {
         return ApiResponse.onSuccess(characterService.createEquipment(characterId, request));
