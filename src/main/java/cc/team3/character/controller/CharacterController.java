1512b7a8e8fa0e70737071fcb3ff98b81ce76446
m020202 <m020202@naver.com>
2025-06-05 15:33:41 +0900

    [REFACTOR] #20: 캐릭터 생성 API Path Variable 추가

--- src/main/java/cc/team3/character/controller/CharacterController.java
+++ src/main/java/cc/team3/character/controller/CharacterController.java
@@ -5,10 +5,7 @@ import cc.team3.character.dto.CharacterResponse;
 import cc.team3.character.service.CharacterService;
 import cc.team3.global.apiPayload.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CharacterController {
     private final CharacterService characterService;
 
-    @PostMapping
-    public ApiResponse<CharacterResponse.CharacterDetailsResponseDTO> createCharacter(@RequestBody CharacterRequest.CharacterCreateRequestDTO request) {
-        return ApiResponse.onSuccess(characterService.createCharacter(request));
+    @PostMapping("/{userId}")
+    public ApiResponse<CharacterResponse.CharacterDetailsResponseDTO> createCharacter(@RequestParam("userId") Long userId,  @RequestBody CharacterRequest.CharacterCreateRequestDTO request) {
+        return ApiResponse.onSuccess(characterService.createCharacter(userId, request));
     }
 }
