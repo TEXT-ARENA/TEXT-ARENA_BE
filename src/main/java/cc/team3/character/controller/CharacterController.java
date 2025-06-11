31f001cb63c7f2e2b92d6b1a68d1ee8bec210892
m020202 <m020202@naver.com>
2025-06-11 10:43:05 +0900

    [REFACTOR] #20: API 요청 파라미터 수정

--- src/main/java/cc/team3/character/controller/CharacterController.java
+++ src/main/java/cc/team3/character/controller/CharacterController.java
@@ -15,14 +15,14 @@ public class CharacterController {
     private final CharacterService characterService;
 
     @PostMapping("/{userId}")
-    public ApiResponse<CharacterResponse.CharacterDetailsResponseDTO> createCharacter(@RequestParam("userId") Long userId, @RequestBody CharacterRequest.CharacterCreateRequestDTO request) {
+    public ApiResponse<CharacterResponse.CharacterDetailsResponseDTO> createCharacter(@PathVariable("userId") Long userId, @RequestBody CharacterRequest.CharacterCreateRequestDTO request) {
         return ApiResponse.onSuccess(characterService.createCharacter(userId, request));
     }
 
     @Operation(summary = "장비 생성", description = "장비를 생성하고자 하는 캐릭터  ID를 넣어주세요 ! <br><br>" +
         "equipmentType: 무기, 모자, 상의, 신발 중 하나 입력해주세요 ! <br> equipmentName: 장비 이름 <br> description: 장비 설명")
     @PostMapping("/{characterId}/equipments")
-    public ApiResponse<CharacterResponse.CharacterDetailsResponseDTO> createWeapon(@RequestParam("characterId") Long characterId, @RequestBody CharacterRequest.CreateEquipmentRequestDTO request) {
+    public ApiResponse<CharacterResponse.CharacterDetailsResponseDTO> createWeapon(@PathVariable("characterId") Long characterId, @RequestBody CharacterRequest.CreateEquipmentRequestDTO request) {
         return ApiResponse.onSuccess(characterService.createEquipment(characterId, request));
     }
 }
