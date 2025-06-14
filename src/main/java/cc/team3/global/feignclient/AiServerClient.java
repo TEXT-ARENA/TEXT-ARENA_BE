a73978f4ad9dc5e88f0a0f0e24b8887fc7062302
m020202 <m020202@naver.com>
2025-06-14 16:35:33 +0900

    [REFACTOR]: Ai 서버 도메인 수정

--- src/main/java/cc/team3/global/feignclient/AiServerClient.java
+++ src/main/java/cc/team3/global/feignclient/AiServerClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "aiServerClient", url = "http://35.153.144.33:8000")
+@FeignClient(name = "aiServerClient", url = "https://62u2ykms2i.execute-api.us-east-1.amazonaws.com/inha-pj-03")
 public interface AiServerClient {
 
     @PostMapping("/api/characters")
